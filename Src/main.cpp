5319e39c6602a618412ce1accbf05312bea10351
Samuel Gauthier <33706474+SamGauths@users.noreply.github.com>
2017-12-02 13:01:33 -0500

    Update main.cpp

--- Src/main.cpp
+++ Src/main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
 {
 	Godel::GDL *Gobj = new Godel::GDL(hInstance, "GDL Application", DEFAULT_POS, DEFAULT_POS, 640, 480);  // Create a window
-    Gobj->showCursor(0);  // Disable the cursor
+    	Gobj->showCursor(0);  // Disable the cursor
 	Gobj->update();  // Update the state of the window
-    delete Gobj;
+    	delete Gobj;
 	return 0;
 }
