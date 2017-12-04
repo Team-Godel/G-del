aefc1c8f7cf0f3f2eb54ff5b60e72a9b1433dbd0
Samuel Gauthier <33706474+SamGauths@users.noreply.github.com>
2017-12-04 15:18:46 -0500

    Update main.cpp

--- Src/main.cpp
+++ Src/main.cpp
@@ -4,16 +4,27 @@ using namespace std;
 
 int main(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
 {
-	Godel::GDL *Gobj = new Godel::GDL(hInstance, "GDL Application", DEFAULT_POS, DEFAULT_POS, 640, 480);  // Create a window
+    int x = 0;
+    int y = 0;
 
-    	Godel::GodelHelp("CONSTANTS");
-    	Godel::GodelHelp("METHODS");
+    Godel::GDL *Gobj = new Godel::GDL(hInstance, "GDL Application", DEFAULT_POS, DEFAULT_POS, 640, 480);  // Create a window
 
-        Gobj->showCursor(0);  // Disable the cursor
-	Gobj->setCursorPosition(0, 0);  // Set the cursor to a defined position
-	Gobj->update();  // Update the statement of the window
+    Godel::GodelHelp("CONSTANTS");
+    Godel::GodelHelp("METHODS");
 
-        delete Gobj;
+    Gobj->showCursor(0);  // Disable the cursor
+    Gobj->setCursorPosition(110, 120);  // Set the cursor to a defined position
 
-	return 0;
+    Gobj->GetCursorPositionX(&x);  // Get cursor x position by using a pointer or returning the value
+    Gobj->GetCursorPositionY(&y);  // Get cursor y position by using a pointer or returning the value
+
+    std::cout << x << std::endl;
+    std::cout << y << std::endl;
+
+    Gobj->update();  // Update the statement of the window
+
+
+    delete Gobj;
+
+    return 0;
 }
