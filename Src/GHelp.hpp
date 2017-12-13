aeb6c34ea4d49745e442f79ff540fdc79b1ea303
Samuel Gauthier <33706474+SamGauths@users.noreply.github.com>
2017-12-13 14:00:23 -0500

    Update GHelp.hpp

--- Src/GHelp.hpp
+++ Src/GHelp.hpp
@@ -19,6 +19,8 @@ std::string Const[NB_CONSTANTS] = {
 "#define DEFAULT_POS CW_USEDEFAULT",
 "#define DEFAULT_WIDTH 640",
 "#define DEFAULT_HEIGHT 480",
+"#define METHODS_LIST \"METHODS\"",
+"#define CONSTANTS_LIST \"CONSTANTS\"",
 "const int NB_METHODS = 4;",
 "const int NB_CONSTANTS = 6;"};
 
