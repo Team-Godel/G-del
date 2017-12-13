d549ba4da399afd6eb9ffefe2f420feae07e17cd
Samuel Gauthier <33706474+SamGauths@users.noreply.github.com>
2017-12-13 13:54:48 -0500

    Update GConsts.hpp

--- Src/GConsts.hpp
+++ Src/GConsts.hpp
@@ -4,7 +4,7 @@
 #include "GWindow.hpp"
 // Preprocessor directives
 #define CLASS "gdlWindow"
-#define DEFAULT_POS CW_USEDEFAULT
+#define DEFAULT_WIN_POS CW_USEDEFAULT
 #define DEFAULT_WIDTH 640
 #define DEFAULT_HEIGHT 480
 #define METHODS_LIST "METHODS"
