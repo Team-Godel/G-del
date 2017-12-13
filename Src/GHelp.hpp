d72de1cd41e57ba8e6f30bffe01c80ece3b68f38
Samuel Gauthier <33706474+SamGauths@users.noreply.github.com>
2017-12-13 14:16:51 -0500

    Update GHelp.hpp

--- Src/GHelp.hpp
+++ Src/GHelp.hpp
@@ -8,44 +8,45 @@
 
 namespace gdl
 {
-std::string Meth[NB_METHODS] = {
-"GDL(HINSTANCE instance, LPSTR title, int x, int y, int w, int h) \n- Create a window",
-"update() \n- Update the state of a window",
-"showCursor(bool onOff) \n- Show or not the cursor",
-"setCursorPosition(int x, int y) \n- Set the cursor to a defined position"};
+std::string Obj[NB_OBJECTS] = {
+"GWindow \n",
+"GItem \n",
+"GButton \n"};
 
 std::string Const[NB_CONSTANTS] = {
 "#define CLASS \"gdlWindow\"",
 "#define DEFAULT_WIN_POS CW_USEDEFAULT",
 "#define DEFAULT_WIDTH 640",
 "#define DEFAULT_HEIGHT 480",
-"#define METHODS_LIST \"METHODS\"",
+"#define OBJECTS_LIST \"OBJECTS\"",
 "#define CONSTANTS_LIST \"CONSTANTS\"",
-"const int NB_METHODS = 4;",
+"const int NB_OBJECTS = 3;",
 "const int NB_CONSTANTS = 6;"};
 
 void GHelp(std::string topic)
 {
-    if(topic == "METHODS"){   // Show all the methods
-        std::cout << "Methods" << std::endl << std::endl << std::endl;
+    if(topic == "OBJECTS")   // Show all the methods
+    {
+        std::cout << "Objects" << std::endl << std::endl << std::endl;
 
-        for(int iMethod = 0; iMethod < NB_METHODS; iMethod ++){
-          std::cout << Meth[iMethod] << std::endl << std::endl;
+        for(int iObj = 0; iObj < NB_OBJECTS; iObj ++){
+          std::cout << Obj[iObj] << std::endl;
 
-          if(iMethod == NB_METHODS - 1)
+          if(iObj == NB_OBJECTS - 1)
             std::cout << std::endl << std::endl << std::endl;
         }
     }
-    else if(topic == "CONSTANTS"){  // Show all the constants
+    else if(topic == "CONSTANTS")  // Show all the constants
+    {
         std::cout << "Constants" << std::endl << std::endl << std::endl;
 
         for(int iConst = 0; iConst < NB_CONSTANTS; iConst ++){
           std::cout << Const[iConst] << std::endl << std::endl;
 
-          if(iConst == NB_CONSTANTS - 1) 
-            std::cout << std::endl << std::endl << std::endl; 
+          if(iConst == NB_CONSTANTS - 1)
+            std::cout << std::endl << std::endl << std::endl;
         }
     }
-}  
+}
 }
 #endif // HELP_H_INCLUDED
