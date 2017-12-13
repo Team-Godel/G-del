b8b75677ec5d7755f095420a8fc4d93f1db57dfe
Samuel Gauthier <33706474+SamGauths@users.noreply.github.com>
2017-12-13 13:50:58 -0500

    Update Entry_Point.cpp

--- Src/Entry_Point.cpp
+++ Src/Entry_Point.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 int main(HINSTANCE instance, HINSTANCE previousInstance, LPTSTR lpCommandLine, int commandLineShow)
 {
-    gdl::GWindow* window = new gdl::GWindow(instance, "GDL Window", 100, 100, 600, 400);
+    gdl::GWindow* window = new gdl::GWindow(instance, "GDL Window", DEFAULT_POS, DEFAULT_POS, DEFAULT_WIDTH, DEFAULT_HEIGHT);
     gdl::GButton* button = new gdl::GButton();
-    
+
     gdl::GodelHelp(METHODS_LIST);
     gdl::GodelHelp(CONSTANTS_LIST);
-    
+
     window->setCommandLine(commandLineShow);
     button->link(window);
     window->run();
