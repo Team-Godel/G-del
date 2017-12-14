7b81390f4ecaf12bbe6f9845b6c06faa5ab8b795
Samuel Gauthier <33706474+SamGauths@users.noreply.github.com>
2017-12-13 23:57:38 -0500

    Update GHelp.hpp

--- Src/GHelp.hpp
+++ Src/GHelp.hpp
@@ -24,7 +24,7 @@ std::string Const[NB_CONSTANTS] = {
 
 void GHelp(std::string topic)
 {
-    if(topic == "OBJECTS")   // Show all the methods
+    if(topic == "OBJECTS")   // Show all the objects
     {
         std::cout << "Objects" << std::endl << std::endl << std::endl;
 
@@ -47,5 +47,40 @@ void GHelp(std::string topic)
         }
     }
 }
+
+void GGetMethodInfos(std::string method)  // Show the details of a method
+{
+    if(method == "GWindow"){
+        std::cout << "Class: GWindow\nGWindow(HINSTANCE, LPSTR, int, int, int, int); //Constructor. Create a window\n\n" << std::endl;
+    }
+    else if(method == "~GWindow"){
+        std::cout << "Class: GWindow\n~GWindow(); //Destructor\n\n" << std::endl;
+    }
+    else if(method == "showCursor"){
+        std::cout << "Class: GWindow\nvoid showCursor(bool); //Enable or disable the cursor\n\n" << std::endl;
+    }
+    else if(method == "setCursorPosition"){
+        std::cout << "Class: GWindow\nvoid setCursorPosition(int, int); //Set the cursor to a defined position\n\n" << std::endl;
+    }
+    else if(method == "getCursorPositionX"){
+        std::cout << "Class: GWindow\nint getCursorPositionX(int*); //Get cursor's X position\n\n" << std::endl;
+    }
+    else if(method == "getCursorPositionY"){
+        std::cout << "Class: GWindow\nint getCursorPositionY(int*); //Get cursor's Y position\n\n" << std::endl;
+    }
+    else if(method == "run"){
+        std::cout << "Class: GWindow\nvoid run(); //Update the current state of the window\n\n" << std::endl;
+    }
+    else if(method == "GHelp"){
+        std::cout << "Class: None\nvoid GHelp(std::string topic); //Show a list of all the objects or constants\n\n" << std::endl;
+    }
+    else if(method == "GGetMethodInfos"){
+        std::cout << "Class: None\nvoid GGetMethodInfos(std::string method); //Show the informations of a method\n\n" << std::endl;
+    }
+    else{
+        std::cout << "This method doesn't exist...\n\n" << std::endl;
+    }
 }
+}
+
 #endif // HELP_H_INCLUDED
