a08b78955e486b90f0cd3b47930a9528338b1131
Samuel Gauthier <33706474+SamGauths@users.noreply.github.com>
2017-12-14 00:00:23 -0500

    Update Entry_Point.cpp

--- Src/Entry_Point.cpp
+++ Src/Entry_Point.cpp
@@ -9,9 +9,9 @@ int main(HINSTANCE instance, HINSTANCE previousInstance, LPTSTR lpCommandLine, i
     gdl::GWindow* window = new gdl::GWindow(instance, "GDL Window", DEFAULT_WIN_POS, DEFAULT_WIN_POS, DEFAULT_WIDTH, DEFAULT_HEIGHT);
     gdl::GButton* button = new gdl::GButton();
 
-    gdl::GHelp(OBJECTS_LIST);
-    gdl::GHelp(CONSTANTS_LIST);
-    gdl::GGetMethodInfos("GWindow");
+    gdl::GHelp(OBJECTS_LIST);  // Show the list of all the objects
+    gdl::GHelp(CONSTANTS_LIST);  // Show the list of all the constants
+    gdl::GGetMethodInfos("GWindow");  // Get the infos of a method
 
     window->setCommandLine(commandLineShow);
     button->link(window);
